c6cc50aa0e4d26d8a49657c44cfd0afd54ca9df4
frive85 <front05@gmx.de>
2025-09-25 19:13:24 +0200

    added code to crate year,month,day,weekday for every date_columns

--- clean.py
+++ clean.py
@@ -1,20 +1,34 @@
 def clean_cols(df):
-    """Strips whitespace, converts to lowercase, and replaces spaces with underscores."""
+    """standardize column names of a given df
+    """
     df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
     return df
 
 def clean_data(orders, campaign, process, intern):
+    """Cleans our 4 Dataframes from the Sales in terms of:
+    - Standardizing Column Names
+    - Drop Duplicates depending on order_id in every DF
+    - Crate year,month,day,weekday for every date_columns
+    """
+    # standardize column names
     orders = clean_cols(orders)
     campaign = clean_cols(campaign)
     process = clean_cols(process)
     intern = clean_cols(intern)
 
-# Handle Duplicates: The raw data may have multiple rows per order (e.g., for each product).
-# For our process analysis, we only need one unique entry per order.
+    # drop duplicates by order_id 
     orders.drop_duplicates(subset='order_id', keep='first', inplace=True)
     process.drop_duplicates(subset='order_id', keep='first', inplace=True)
     intern.drop_duplicates(subset='order_id', keep='first', inplace=True)
     campaign.drop_duplicates(subset='order_id', keep='first', inplace=True)
+
+    for df in [orders, campaign, process, intern]:
+        for i in df.columns:
+            if 'date' in i:
+                df[f"{i}_year"]=df[i].dt.year
+                df[f"{i}_month"]=df[i].dt.month
+                df[f"{i}_weekday"]=df[i].dt.dayofweek
+                df[f"{i}_day"]=df[i].dt.day
 
     return orders, campaign, process, intern
     
